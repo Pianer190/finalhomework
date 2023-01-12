10a7ae6762883d3006df702683601b31c95f2319
Dmitry <pianer190@mail.ru>
2023-01-12 08:51:38 +0300

    Task2 complete.

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine());
+while(true){
+    if(m >= n){
+        Console.WriteLine("Значение M не может быть больше или равно N!");
+        Console.Write("Введите значение M: ");
+        m = int.Parse(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        n = int.Parse(Console.ReadLine());
+        continue;
+    } 
+    break;
+}
+int result = 0;
+for(; m <= n; m++){
+    if(m < 1) continue;
+    result += m;
+}
+Console.WriteLine("Результат: " + result);
