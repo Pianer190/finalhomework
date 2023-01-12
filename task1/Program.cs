19cd418316e08962b17cb1c3647a76f56443d5a6
Dmitry <pianer190@mail.ru>
2023-01-12 08:32:41 +0300

    Task1 complete

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+void MyFunction(int n){
+    Console.Write(n + " ");
+    if(n > 1) MyFunction(n - 1);
+}
+
+    Console.Write("Введите число: ");
+int input = int.Parse(Console.ReadLine());
+
+MyFunction(input);
